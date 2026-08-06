0cd12365ba1d404daf55f0a09086bf4038f8e358
Cyu <tasterossa3552980@gmail.com>
2026-08-06 16:09:27 +0800

    big dog charge anim 0.1

--- src/main/java/com/test/testmod/Item/bigDog.java
+++ src/main/java/com/test/testmod/Item/bigDog.java
@@ -7,6 +7,7 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import software.bernie.geckolib.animatable.GeoItem;
@@ -16,6 +17,7 @@ import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInst
 import software.bernie.geckolib.core.animation.AnimatableManager;
 import software.bernie.geckolib.core.animation.AnimationController;
 import software.bernie.geckolib.core.animation.RawAnimation;
+import software.bernie.geckolib.core.object.DataTicket;
 import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.util.GeckoLibUtil;
 import software.bernie.geckolib.animatable.SingletonGeoAnimatable;
@@ -34,9 +36,22 @@ public class bigDog extends Item implements GeoItem {
 
     public void registerControllers(AnimatableManager.ControllerRegistrar controll) {
         AnimationController<bigDog> controller =(new AnimationController<>
-                (this, "atk_controller", 0, stack
-                ->PlayState.STOP));
+                (this, "atk_controller", 2, state
+                -> {
+                    ItemStack stack=state.getData(software.bernie.geckolib.constant.DataTickets.ITEMSTACK);
+
+                    if(stack != null && stack.hasTag() && stack.getTag().getBoolean("charge")){
+                        net.minecraft.client.player.LocalPlayer user = net.minecraft.client.Minecraft.getInstance().player;
+                        if (user != null && user.isUsingItem() && user.getUseItem() == stack){
+                            return state.setAndContinue(RawAnimation.begin().thenPlay("animation.big_dog.charge"));
+                        }
+                    }
+                    state.getController().forceAnimationReset();
+
+                    return PlayState.STOP;
+                }));
                 controller.triggerableAnim("charge", RawAnimation.begin().thenPlay("animation.big_dog.charge"));
+                //controller.triggerableAnim("return", RawAnimation.begin().thenPlay("animation.big_dog.return"));
                 controll.add(controller);
     }
 
@@ -44,56 +59,49 @@ public class bigDog extends Item implements GeoItem {
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return cache;
     }
- /*   @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player p, InteractionHand hand) {
-        ItemStack itemstack = p.getItemInHand(hand);
-
-        if (!level.isClientSide()) {
-            this.triggerAnim(p, GeoItem.getOrAssignId(itemstack, (net.minecraft.server.level.ServerLevel) level),
-                    "atk_controller", "charge");
-        }
-
-        return InteractionResultHolder.sidedSuccess(itemstack, level.isClientSide());
-    }
-  */
 
     @Override
     public InteractionResultHolder<ItemStack> use (Level level, Player p, InteractionHand hand){
         ItemStack itemstack = p.getItemInHand(hand);
 
-        if(!level.isClientSide()) {
-            this.triggerAnim(p,GeoItem.getOrAssignId(itemstack,
-                            (net.minecraft.server.level.ServerLevel)level),
-                    "atk_controller","charge");
-
-        }
+        itemstack.getOrCreateTag().putBoolean("charge", true);
         p.startUsingItem(hand);
         return InteractionResultHolder.consume(itemstack);
     }
 
+
     @Override
     public int getUseDuration(ItemStack itemstack){
-        return 100;
+        return 72000;
+    }
+    @Override
+    public UseAnim getUseAnimation(ItemStack stack) {
+        return UseAnim.NONE;
     }
     @Override
     public void releaseUsing(ItemStack itemstack, Level level, LivingEntity livingEntity, int i) {
         if(!level.isClientSide()&& livingEntity instanceof Player p) {
             int charge = this.getUseDuration(itemstack)- i;
 
-            if (charge >= 80) {
+            if(itemstack.hasTag()){
+                itemstack.getTag().putBoolean("charge",false);
+            }
+
+            /*if (charge >= 80) {
                 this.triggerAnim(p,
                         GeoItem.getOrAssignId(itemstack,(net.minecraft.server.level.ServerLevel)level),
                         "atk_controller","charge");
-            }
+            }*/
+            //這個是之後要放攻擊效果的地方
         }
     }
 
 
     @Override
     public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
-        if(attacker.level().isClientSide){
-            this.triggerAnim(attacker, GeoItem.getOrAssignId(stack,
-                    (net.minecraft.server.level.ServerLevel) attacker.level()), "atk_controller", "attack");
+        if(!attacker.level().isClientSide() && attacker instanceof Player p){
+            this.triggerAnim(p, GeoItem.getOrAssignId(stack,
+                    (net.minecraft.server.level.ServerLevel) attacker.level()), "return", "animation.big_dog.return");
         }
         return super.hurtEnemy(stack, target, attacker);
         }
